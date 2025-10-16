21833bf1b757e084c5c4fdb8d4cc3cd7e71fb8ee
rubrick <rubrick@mateos.local>
2025-10-16 19:01:29 +0200

    Eliminar webapps: Basecamp, HEY, Figma, Zoom, Discord y X

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,5 +1,3 @@
-mateos-webapp-install "HEY" https://app.hey.com HEY.png
-mateos-webapp-install "Basecamp" https://launchpad.37signals.com Basecamp.png
 mateos-webapp-install "WhatsApp" https://web.whatsapp.com/ WhatsApp.png
 mateos-webapp-install "Google Photos" https://photos.google.com/ "Google Photos.png"
 mateos-webapp-install "Google Contacts" https://contacts.google.com/ "Google Contacts.png"
@@ -7,7 +5,3 @@ mateos-webapp-install "Google Messages" https://messages.google.com/web/conversa
 mateos-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
 mateos-webapp-install "YouTube" https://youtube.com/ YouTube.png
 mateos-webapp-install "GitHub" https://github.com/ GitHub.png
-mateos-webapp-install "X" https://x.com/ X.png
-mateos-webapp-install "Figma" https://figma.com/ Figma.png
-mateos-webapp-install "Discord" https://discord.com/channels/@me Discord.png
-mateos-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "mateos-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
