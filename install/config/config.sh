560c5b5480d4f0e7841d9e4e25ba2416591220dd
rubrick <rubrick@mateos.local>
2025-10-16 19:01:14 +0200

    Migrar de bash a zsh con oh-my-zsh

--- install/config/config.sh
+++ install/config/config.sh
@@ -2,5 +2,8 @@
 mkdir -p ~/.config
 cp -R ~/.local/share/mateos/config/* ~/.config/
 
-# Use default bashrc from MateOS
-cp ~/.local/share/mateos/default/bashrc ~/.bashrc
+# Use default zshrc from MateOS
+cp ~/.local/share/mateos/default/zshrc ~/.zshrc
+
+# Set zsh as default shell
+chsh -s /usr/bin/zsh
